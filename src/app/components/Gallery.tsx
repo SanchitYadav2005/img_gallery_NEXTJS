2262414e72450bf6aaec45564ea74e06b27a3a69
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-27 23:37:52 +0530

    trying to fix the issue

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -7,9 +7,9 @@ const Gallery = async () => {
   const url = "https://api.pexels.com/v1/curated";
   const images: ImagesResult | undefined = await fetchImages(url);
   if (!images)
-    return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
+    return <h2 className="my-4 text-2xl font-bold">No Images Found</h2>;
   return (
-    <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
+    <section className="px-2 my-4 grid gap-2 grid-cols-gallery">
       {images.photos.map((photo) => (
         <ImgContainer photo={photo} key={photo.id}/>
       ))}
--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -1,19 +1,20 @@
-import type {Photo} from '@/models/Images';
-import Image from 'next/image';
+import type { Photo } from "@/models/Images";
+import Image from "next/image";
 
 type Props = {
-    photo: Photo
-}
+  photo: Photo;
+};
 
-export default function ImgContainer({photo}: Props){
-    return(
-        <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
-            <Image
-                src={photo.src.large}
-                alt={photo.alt}
-                width={250}
-                height={250}
-            />
-        </div>
-    )
+export default function ImgContainer({ photo }: Props) {
+  return (
+    <div key={photo.id} className="h-64 bg-gray-200 rounded-xl relative">
+      <Image
+        src={photo.src.large}
+        alt={photo.alt}
+        fill={true}
+        sizes="(max-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        className="object-cover"
+      />
+    </div>
+  );
 }
